9c3f62ee7c3891bb0ab1ba381e5610717a092f69
karrabi <30956008+karrabi@users.noreply.github.com>
2021-12-07 15:13:25 +0330

    redis added as channel layer

--- taxi/settings.py
+++ taxi/settings.py
@@ -136,3 +136,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+REDIS_URL = 'redis://localhost:6379'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [REDIS_URL],
+        },
+    },
+}
