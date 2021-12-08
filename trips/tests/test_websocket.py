6b9112520ca4b0768d9751987dbbd16fbaeeb090
karrabi <30956008+karrabi@users.noreply.github.com>
2021-12-08 17:30:50 +0330

    send and receive message test added

--- trips/tests/test_websocket.py
+++ trips/tests/test_websocket.py
@@ -23,19 +23,18 @@ class TestWebSocket:
         assert connected is True
         await communicator.disconnect()
 
-
-async def test_can_send_and_receive_messages(self, settings):
-    settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
-    communicator = WebsocketCommunicator(
-        application=application,
-        path='/taxi/'
-    )
-    connected, _ = await communicator.connect()
-    message = {
-        'type': 'echo.message',
-        'data': 'This is a test message.',
-    }
-    await communicator.send_json_to(message)
-    response = await communicator.receive_json_from()
-    assert response == message
-    await communicator.disconnect()
+    async def test_can_send_and_receive_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        await communicator.send_json_to(message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
